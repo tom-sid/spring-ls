50896f1aa0b4187663a9b9fa3f6e5f05540952ac
tomsid <tom_sid@yahoo.com>
2017-08-07 22:43:16 -0500

    Transaction Management

--- demo-data/src/main/java/com/example/demodata/service/GreetingServiceImpl.java
+++ demo-data/src/main/java/com/example/demodata/service/GreetingServiceImpl.java
@@ -4,11 +4,14 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demodata.model.Greeting;
 import com.example.demodata.repository.GreetingRepository;
 
 @Service
+@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 public class GreetingServiceImpl implements GreetingService {
 
 	@Autowired
@@ -25,14 +28,24 @@ public class GreetingServiceImpl implements GreetingService {
 	}
 
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 	public Greeting create(Greeting greeting) {
 		if (greeting.getId() != null) {
 			return null;
 		}
-		return greetingRepository.save(greeting);
+		Greeting savedGreeting = greetingRepository.save(greeting);
+		
+		// Illustrate Transaction rollback
+		
+		if(savedGreeting.getId() == 4L){
+			throw new RuntimeException();
+		}
+		
+		return savedGreeting;
 	}
 
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 	public Greeting update(Greeting greeting) {
 		Greeting oldGreeting = greetingRepository.findOne(greeting.getId());
 		if (oldGreeting == null) {
@@ -42,6 +55,7 @@ public class GreetingServiceImpl implements GreetingService {
 	}
 
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 	public void delete(Long id) {
 		greetingRepository.delete(id);
 	}
